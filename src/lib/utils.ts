f45a87a766d985550bbb3f25b2d990d5ec60fcd7
GiacomoSchinco <84517547+GiacomoSchinco@users.noreply.github.com>
2026-02-10 13:00:36 +0100

    inserimento shadcn/ui

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,1 +1,6 @@
-// Funzioni di utilità generali
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
